79d1f26bf9f33b6a9dafc729b2522618f5c7b3be
Anderson Nunes <andersondngto@gmail.com>
2022-03-02 08:09:53 -0300

    config install luxon and jwt config

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import "./App.css";
 import { BrowserRouter } from "react-router-dom";
 import AppRoutes from "./lib/routes";
@@ -7,30 +6,32 @@ import { Layout } from "antd";
 import AppSlider from "./components/Slider";
 import { Content, Footer, Header } from "antd/lib/layout/layout";
 import api from "./lib/api";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
-  const fetcher = (url: string) => api(url).then(r => r.data)
+  const fetcher = (url: string) => api(url).then((r) => r.data);
 
   return (
-    
     <SWRConfig value={{ fetcher: fetcher }}>
-      <BrowserRouter>
-        <Layout style={{ minHeight: "100vh" }}>
-          <AppSlider />
-          <Layout>
-            <Header
-              className="site-layout-sub-header-background"
-              style={{ padding: 0 }}
-            />
-            <Content style={{ margin: "24px 16px 0" }}>
-              <AppRoutes />
-            </Content>
-            <Footer style={{ textAlign: "center" }}>
-              Sasho ©2021 Desenvolvido por Anderson Nunes{" "}
-            </Footer>
+      <AuthProvider>
+        <BrowserRouter>
+          <Layout style={{ minHeight: "100vh" }}>
+            <AppSlider />
+            <Layout>
+              <Header
+                className="site-layout-sub-header-background"
+                style={{ padding: 0 }}
+              />
+              <Content style={{ margin: "24px 16px 0" }}>
+                <AppRoutes />
+              </Content>
+              <Footer style={{ textAlign: "center" }}>
+                Sasho ©2021 Desenvolvido por Anderson Nunes{" "}
+              </Footer>
+            </Layout>
           </Layout>
-        </Layout>
-      </BrowserRouter>
+        </BrowserRouter>
+      </AuthProvider>
     </SWRConfig>
   );
 }
