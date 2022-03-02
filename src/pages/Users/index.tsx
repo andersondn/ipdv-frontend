9699e2b680352e063c91a7a283670bd486544362
Anderson Nunes <andersondngto@gmail.com>
2022-03-02 08:24:50 -0300

    feature: list users

--- src/pages/Users/index.tsx
+++ src/pages/Users/index.tsx
@@ -1,17 +1,85 @@
+import { Button, Row, Space, Table, Tooltip } from "antd";
 import React from "react";
+import { PlusOutlined, DeleteTwoTone, EditTwoTone } from '@ant-design/icons';
+import { Link } from "react-router-dom";
 import useSWR from "swr";
 
 // import { Container } from './styles';
 
 const Users: React.FC = () => {
   const { data: users, error } = useSWR("/users");
+  const isDataLoading = !error && !users
+
+
+	const actionsButtons = (text: string, user: any) => (
+		<Space>
+			<Tooltip title="Editar">
+				<Link to={`/users/edit/${user.id}`}>
+					<Button
+						shape="circle"
+						icon={<EditTwoTone />}
+					/>
+				</Link>
+			</Tooltip>
+			<Tooltip title="Apagar">
+				<Button
+					shape="circle"
+					// onClick={() => deleteUser(user._id)}
+					icon={<DeleteTwoTone twoToneColor="#bb0e0e" />}
+					danger
+				/>
+			</Tooltip>
+		</Space>
+	)
+
+  const columns: any = [
+		{
+			title: 'Nome',
+			dataIndex: 'name',
+
+		},
+		{
+			title: 'E-mail',
+			dataIndex: 'email',
+
+		},
+		{
+			title: 'Cargo',
+			dataIndex: 'role',
+		},
+    {
+      title: 'Departamento',
+      dataIndex: 'department_id',
+    },
+		{
+			title: 'Ações',
+			key: 'actions',
+			width: 20,
+			render: actionsButtons
+		},
+	]
 
   return (
     <>
-      <h1>Users page</h1>
+      <h1>Usuários</h1>
 
-      {JSON.stringify(users)}
-      {JSON.stringify(error)}
+      <Row justify='end'>
+				<Link to="/users/add">
+
+					<Button icon={<PlusOutlined />}>Adicionar</Button>
+				</Link>
+
+			</Row>
+			<Table
+				columns={columns}
+				dataSource={users}
+				rowKey="id"
+				loading={isDataLoading}
+			/>
+
+
+
+
     </>
   );
 };
